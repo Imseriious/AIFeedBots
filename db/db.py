3564811d4ee20629c0bf1ae74428e55db6dd2cb5
Samuel Pop <47713133+Imseriious@users.noreply.github.com>
2024-12-10 14:21:43 +0100

    Update db.py

--- db/db.py
+++ db/db.py
@@ -1,11 +1,8 @@
 from pymongo import MongoClient
 def get_database():
- 
-   # Provide the mongodb atlas url to connect python to mongodb using pymongo
-   CONNECTION_STRING = "mongodb+srv://samserver:sam4321@aifeed.4vxtg.mongodb.net/dev?retryWrites=true&w=majority&appName=aifeed"
- 
+  
    # Create a connection using MongoClient. You can import MongoClient or use pymongo.MongoClient
-   client = MongoClient(CONNECTION_STRING)
+   client = MongoClient(//FromENV)
    mydb = client["dev"]
    return mydb
    # Create the database for our example (we will use the same database throughout the tutorial
